2d98e7caee34cf2cbf6762274a4a189482332af7
Maria Miroshnichenko <marg13@yandex.ru>
2017-01-11 20:53:44 +0300

    добавила нового провадера

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,31 +18,9 @@ class AppServiceProvider extends ServiceProvider
     {
       
         $path = $request->path();
-        $menu = Menu::handler('nav', ['id' => 'nav']); 
-
-// items
-$menu
-    ->add('/', 'Главная')
-    ->add('article/add', 'Новая статья')
-
-    ->raw(null, null, ['class' => 'divider'])
-    ->add('', 'Админ', Menu::items() 
-        ->prefixParents() 
-        ->add('/admin/articles', 'Статьи') // with prefix: /folders/urgent
-        ->add('/admin/users', 'Пользователи')
-    );
-
-// styling
-$menu
-    ->addClass('nav navbar-nav')
-    ->addId()
-    ->getItemsByContentType(Menu\Items\Contents\Link::class)
-    ->map(function($item) {
-        if ( $item->isActive() )  {
-            $item->addClass('active');
-        }
-    });
-        View::share('menu', $menu);
+        
+        View::share('path', $path);
+        View::share('auth', false);
 
 
     }
