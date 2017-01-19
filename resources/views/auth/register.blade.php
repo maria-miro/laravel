c69e6338bb6c25858ea544197adae97ee1915a60
Maria Miroshnichenko <marg13@yandex.ru>
2017-01-19 20:31:36 +0300

    Разделила валидацию

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,27 +6,37 @@
     <form method="post">
 
      {{ csrf_field() }}
-        @if (count($errors) > 0)
-         <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
         E-mail<br>
         <input type="email" name="email" size="80" value ="{{old('email')}}"><br>
+        @foreach ($errors->get('email') as $message)
+            <ul>
+                <li>{{ $message }}</li>
+            </ul>
+        @endforeach
 
         Имя пользователя<br>
         <input type="text" name="name" size="80" value ="{{old('name')}}"><br>
+        @foreach ($errors->get('name') as $message)
+            <ul>
+                <li>{{ $message }}</li>
+            </ul>
+        @endforeach
 
         Пароль<br>
         <input type="password" name="password" size="80" value =""><br>
+        @foreach ($errors->get('password') as $message)
+            <ul>
+                <li>{{ $message }}</li>
+            </ul>
+        @endforeach
 
         Пароль<br>
         <input type="password" name="password2" size="80" value =""><br>
-
+        @foreach ($errors->get('password2') as $message)
+            <ul>
+                <li>{{ $message }}</li>
+            </ul>
+        @endforeach
          <input type="submit" value="Зарегистрироваться"><br>
     </form>
  @endsection  
