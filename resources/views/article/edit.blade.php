2591e61facd2ee1f9b5f1092f0948d623cb29fef
Maria Miroshnichenko <marg13@yandex.ru>
2017-01-22 20:06:25 +0300

    Исправлен шаблон edit

--- resources/views/article/edit.blade.php
+++ resources/views/article/edit.blade.php
@@ -5,9 +5,19 @@
 
          {{ csrf_field() }}
             Заголовок статьи<br>
+           @foreach ($errors->get('title') as $message)
+	            <ul>
+	                <li>{{ $message }}</li>
+	            </ul>
+        	@endforeach
             <input type="text" name="title" size="80" value ="{{$title}}"><br>
 
             Текст статьи<br>
+            @foreach ($errors->get('content') as $message)
+	            <ul>
+	                <li>{{ $message }}</li>
+	            </ul>
+        	@endforeach
             <textarea name="content"  cols="80" rows="10" >{{$content}}</textarea><br>
 
              <input type="submit" value="Сохранить"><br>
