b40aa60ddedb19b3aa44e49b7ef56585a813d0a8
Maria Miroshnichenko <marg13@yandex.ru>
2017-02-11 20:50:37 +0300

    Ошибка в шаблоне

--- resources/views/article/list.blade.php
+++ resources/views/article/list.blade.php
@@ -1,34 +1,32 @@
 <h1>{{$listName or ''}}</h1><hr>
+
 @forelse ($articles as $article)
+	<article class="entry">
 
+		<header class="entry-header">
 
+			<h3 class="entry-title">
+	    		<a href="{{route('article.one', ['id' => $article->id])}}">{{$article->title}}</a><hr>
+			</h3> 				 
+		
+			<div class="entry-meta">
+				<ul>
+					<li>{{getRusDate($article->updated_at)}}</li>
+					<span class="meta-sep">&bull;</span>
 
-<article class="entry">
-
-	<header class="entry-header">
-
-		<h3 class="entry-title">
-    		<a href="{{route('article.one', ['id' => $article->id])}}">{{$article->title}}</a><hr>
-		</h3> 				 
-	
-		<div class="entry-meta">
-			<ul>
-				<li>{{getRusDate($article->updated_at)}}</li>
-				<span class="meta-sep">&bull;</span>
-
-				<li>{{$article->tagList()}}</li>
-				<span class="meta-sep">&bull;</span>
-				<li>{{$article->user->name}}</li>
-			</ul>
+					<li>{{$article->tagList()}}</li>
+					<span class="meta-sep">&bull;</span>
+					<li>{{$article->user->name}}</li>
+				</ul>
+			</div> 
+		 
+		</header> 
+		
+		<div class="entry-content">
+			<p>{{makeDescription($article->content)}}</p>
 		</div> 
-	 
-	</header> 
-	
-	<div class="entry-content">
-		<p>{{makeDescription($article->content)}}</p>
-	</div> 
 
-</article> <!-- end entry -->
+	</article> <!-- end entry -->
 @empty
     <p>{{trans('articles.no_list')}}</p>
 @endforelse
