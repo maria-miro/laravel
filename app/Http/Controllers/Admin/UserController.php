da96f503e091c4c776a985827b5f8ca26cfc012d
Maria Miroshnichenko <marg13@yandex.ru>
2017-09-04 15:39:37 +0300

    поправлены ошибки

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Models\User;
+use Illuminate\Support\Facades\Cache;
 
 class UserController extends Controller
 {  
